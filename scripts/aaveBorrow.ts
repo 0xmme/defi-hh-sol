fa95eeed25d82d54c75d7bb44ca18cb843978eae
mme <9083787+mme022@users.noreply.github.com>
2022-09-03 19:27:29 +0100

    added some interfaces

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -2,6 +2,7 @@ import { ethers, getNamedAccounts, network } from "hardhat";
 import { getWeth } from "../scripts/getWeth";
 import { networkConfig } from "../helper-hardhat-config";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Address } from "hardhat-deploy/dist/types";
 
 async function main() {
   //await getWeth();
@@ -9,6 +10,21 @@ async function main() {
   const { deployer } = await getNamedAccounts();
   const deploySigner = await ethers.getSigner(deployer);
   const lendingPool = await getLendingPool(deploySigner);
+  const wethToken = networkConfig[network.name].wethToken!;
+
+  async function approveErc20(
+    approveErc: Address,
+    approveAddress: Address,
+    approveAmount: number,
+    approvingAccount: Address
+  ) {
+    const approvingSigner = await ethers.getSigner(approvingAccount);
+    const erc20Contract = await ethers.getContractAt(
+      "IERC20",
+      approveErc,
+      approvingSigner
+    );
+  }
 
   async function getLendingPool(Signer: SignerWithAddress) {
     const aavePoolAddressesProvider = await ethers.getContractAt(
